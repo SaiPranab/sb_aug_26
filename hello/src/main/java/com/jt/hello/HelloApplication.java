8966be4d2ddfecf12a0f34c9f81cd27f064ffa6f
SaiPranab <saipranabpatra1234@gmail.com>
2026-08-13 21:22:07 +0530

    springboot conatiner

---
+++ hello/src/main/java/com/jt/hello/HelloApplication.java
@@ -0,0 +1,21 @@
+package com.jt.hello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+
+@ImportResource("beans.xml")
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(HelloApplication.class, args);
+		Greet greet = context.getBean(Greet.class);
+		greet.sayHello();
+		
+		System.out.println("Hello SpringBoot");
+	}
+}
