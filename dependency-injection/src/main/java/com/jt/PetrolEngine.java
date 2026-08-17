49a925422c6094c08bd5a814c6cd4f67f93fcbeb
SaiPranab <saipranabpatra1234@gmail.com>
2026-08-17 20:43:01 +0530

    dependency Injection

---
+++ dependency-injection/src/main/java/com/jt/PetrolEngine.java
@@ -0,0 +1,20 @@
+package com.jt;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PetrolEngine implements Engine{
+
+  @Override
+  public void startEngine() {
+    // TODO Auto-generated method stub
+    System.out.println("Petrol Engine starting");
+  }
+  
+  @Override
+  public void stopEngine() {
+    // TODO Auto-generated method stub
+    System.out.println("Petrol Engine stopping");
+  }
+  
+}
