8b226464e49d14ee2c9afc9e4a280aa7839682e7
SaiPranab <saipranabpatra1234@gmail.com>
2026-08-22 19:27:53 +0530

    cmd line runner

--- bean-life-cycle/src/main/java/com/jt/BeanLifeCycleApplication.java
+++ bean-life-cycle/src/main/java/com/jt/BeanLifeCycleApplication.java
@@ -1,16 +1,26 @@
 package com.jt;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeanLifeCycleApplication {
+public class BeanLifeCycleApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
+		System.out.println("Hiii");
 		var context = SpringApplication.run(BeanLifeCycleApplication.class, args);
+		System.out.println("Hello");
 		var greet = context.getBean(Greet.class);
 		greet.greet();
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Command Line Runner");
+	}
+
+	
 
 	/*
 		Bean Life Cycle
