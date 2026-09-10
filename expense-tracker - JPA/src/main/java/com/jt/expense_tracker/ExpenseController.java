c979b4a17d239a69c9b7002c0bcd94db2e6c0836
SaiPranab <saipranabpatra1234@gmail.com>
2026-09-10 21:27:13 +0530

    Expense api

--- expense-tracker - JPA/src/main/java/com/jt/expense_tracker/ExpenseController.java
+++ expense-tracker - JPA/src/main/java/com/jt/expense_tracker/ExpenseController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:5173")
 public class ExpenseController {
   private final ExpenseService expenseService;
 
