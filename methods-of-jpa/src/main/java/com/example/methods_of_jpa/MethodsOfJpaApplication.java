b5d7c71e21057a2963a91335923d88050bf60fed
SaiPranab <saipranabpatra1234@gmail.com>
2026-09-16 19:18:36 +0530

    methods of jpa

---
+++ methods-of-jpa/src/main/java/com/example/methods_of_jpa/MethodsOfJpaApplication.java
@@ -0,0 +1,63 @@
+package com.example.methods_of_jpa;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Example;
+
+import lombok.RequiredArgsConstructor;
+
+@SpringBootApplication
+@RequiredArgsConstructor 
+public class MethodsOfJpaApplication {
+	private final ProductRepository productRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MethodsOfJpaApplication.class, args);
+	}
+
+	@Bean 
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			Product product = Product.builder()
+				.productName("Iphone 17 Pro Max")
+				.productBrand("Apple")
+				.productPrice(180000.99)
+				.build();
+
+			// SAVE
+			// Product savedProduct = productRepository.save(product);
+			// System.out.println("saved product is:- " + savedProduct);
+
+			// SAVEALL
+			// productRepository.saveAll(getProducts());
+
+			// COUNT
+			// long totalProducts = productRepository.count();
+			// System.out.println("total number of products is:-" + totalProducts);
+
+			// Exists & Exists By ID
+			// boolean isIphoneExists = productRepository.existsById(100);
+			// System.out.println(" is Iphone 17 exists " + isIphoneExists);
+			
+			// Product existingProduct = productRepository.findById(1).orElseThrow();
+			// boolean isIphoneExists2 = productRepository.exists(Example.of(existingProduct));
+			// System.out.println(" is Iphone 17 exists 2:- " + isIphoneExists2);
+
+			
+		};	
+	}
+
+	private List<Product> getProducts() {
+		return IntStream.range(1, 10).mapToObj(i -> Product.builder()
+				.productName("product-" + i)
+				.productBrand("brand-" + i)
+				.productPrice(10000 * i)
+				.build())
+			.toList();
+	}
+}
---
+++ methods-of-jpa/src/main/java/com/example/methods_of_jpa/Product.java
@@ -0,0 +1,27 @@
+package com.example.methods_of_jpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int productId;
+
+  private String productName;
+
+  private String productBrand;
+
+  private double productPrice;
+}
